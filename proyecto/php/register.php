e9716cd056e104b1456e7882fb76e05fb0258ccb
dd130609 <danieldmejias@gmail.com>
2025-05-22 17:00:07 -0500

    registro con base de datos

---
+++ proyecto/php/register.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'SessionManager.php';
+require_once 'sql.php';
+
+$session = new SessionManager();
+
+    $nom = $_POST['nombre'];
+    $pass = $_POST['pass'];
+    $email = $_POST['correo'];
+    $apell = $_POST['apellido'];
+
+if($nom == "" || $pass == "" || $email == "" || $apell == ""){
+        $mensaje = "Credenciales inválidas. Inténtalo de nuevo.";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else{
+        $insertar = "INSERT INTO admin VALUES('', '$nom', '$apell', '$email', '$pass')";
+        $ejecutarInsertar = mysqli_query($enlace, $insertar);
+        header('Location: ../login.html');
+        $mensaje = "Registro exitoso.";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "</script>";
+}
+
+    
+
+?>
