b0aa7d45256aef9f5f91a1e32f5c3c044ff31986
dd130609 <danieldmejias@gmail.com>
2025-05-23 17:11:50 -0500

    cambios php login

--- proyecto/php/login.php
+++ proyecto/php/login.php
@@ -21,20 +21,38 @@ if ($result->num_rows === 1) {
     $user_data = $result->fetch_assoc(); // Obtiene los datos del usuario como un array asociativo
     $stored_hash = $user_data['contraseña']; // El hash de la contra
 
-    if (password_verify($contra, $stored_hash)) {
+    if($user == "" || $contra == ""){
 
-        $session->login(1, $user);
-        header("location: ../dashboard.html");
-        exit;
-
-    }else{
         $mensaje = "Credenciales inválidas. Inténtalo de nuevo.";
         echo "<script type='text/javascript'>";
         echo "alert('" . $mensaje . "');"; 
         echo "window.history.back();"; 
         echo "</script>";
         exit; 
+
+    }else{
+            if (password_verify($contra, $stored_hash)) {
+
+            $session->login(1, $user);
+            header("location: ../dashboard.html");
+            exit;
+
+        }else{
+            $mensaje = "Credenciales inválidas. Inténtalo de nuevo.";
+            echo "<script type='text/javascript'>";
+            echo "alert('" . $mensaje . "');"; 
+            echo "window.history.back();"; 
+            echo "</script>";
+            exit; 
+        }
     }
+}else{
+        $mensaje = "Credenciales inválidas. Inténtalo de nuevo.";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
 }
 
 ?>
