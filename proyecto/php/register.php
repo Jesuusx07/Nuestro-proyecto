a229a7c6de3c3f3c6fa167d24dca335a3284d94c
dd130609 <danieldmejias@gmail.com>
2025-05-27 14:38:16 -0500

    registrar empleado y verificacion contraseña

--- proyecto/php/register.php
+++ proyecto/php/register.php
@@ -11,6 +11,9 @@ $session = new SessionManager();
 
     $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
 
+    $longMin = 8;
+    $longMax = 40;
+
 if($nom == "" || $pass == "" || $email == "" || $apell == ""){
         $mensaje = "Credenciales inválidas. Inténtalo de nuevo.";
         echo "<script type='text/javascript'>";
@@ -18,12 +21,59 @@ if($nom == "" || $pass == "" || $email == "" || $apell == ""){
         echo "window.history.back();"; 
         echo "</script>";
         exit; 
-}else{
+}else if(strlen($pass) < $longMin){
+        $mensaje = "La contraseña necesita minimo 8 caracteres.";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else if(strlen($pass) > $longMax){
+        $mensaje = "La longitud maxima de caracteres son 40 caracteres.";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else if(!preg_match('/[A-Z]/', $pass)){
+        $mensaje = "La contraseña necesita minimo una mayuscula";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else if(!preg_match('/[a-z]/', $pass)){
+        $mensaje = "La contraseña necesita minimo una minuscula";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else if(!preg_match('/[0-9]/', $pass)){
+        $mensaje = "La contraseña necesita minimo un numero";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}else if(strpos($pass, " ") !== false){
+        $mensaje = "La contraseña no debe contener espacios en blanco";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.history.back();"; 
+        echo "</script>";
+        exit; 
+}
+
+else{
         $insertar = "INSERT INTO admin VALUES('', '$nom', '$apell', '$email', '$pass_hash')";
         $ejecutarInsertar = mysqli_query($enlace, $insertar);
-        header('Location: ../login.html');
-        $mensaje = "Registro exitoso.";
-        echo "$mensaje";
+        $mensaje = "registro exitoso";
+        echo "<script type='text/javascript'>";
+        echo "alert('" . $mensaje . "');"; 
+        echo "window.location.href = '../login.html'"; 
+        echo "</script>";
+
 }
 
     
