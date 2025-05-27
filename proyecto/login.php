5a9febf7f066c8f88f8861d89de5da2c3550b836
dd130609 <danieldmejias@gmail.com>
2025-05-27 17:13:20 -0500

    login arreglado

---
+++ proyecto/login.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once './php/SessionManager.php';
+
+$session = new SessionManager();
+
+    if (!$session->isLoggedIn()){
+    }else{
+        header("location: dashboard.html");
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kenny's - Aplicación Web</title>
+    <link rel="stylesheet" href="./css/login.css">
+</head>
+
+<body>
+    <div class="container">
+        <a href="index.html"> <img class="logo" src="img/Logo Principal (1).png" alt="Kenny's Logo"> </a>
+        
+        <h1>Inicia sesión</h1>
+        <p style="text-align: center;">¿No tienes cuenta? <a class="inc" href="registrarse.html">Registrarse</a></p>
+
+        <div>
+            <form id="login" action="./php/login.php" method="POST">
+                <div class="input-group">
+                    <label for="login_email">Correo electrónico</label>
+                    <input type="email" id="login_email" name="correo" placeholder="tucorreo@gmail.com">
+                </div>
+    
+                <div class="input-group">
+                    <label for="login_password">Contraseña</label>
+                    <input type="password" name="contra" id="login_password">
+                </div>
+                <button type="submit" class="boton-registro" id="login-submit">Iniciar Sesión</button>
+
+                <a href="forgot_password.php" class="Recupera">¿Olvidaste tu contraseña?</a>
+                
+            </form>
+        </div>
+    </div>
+</body>
+</html>
