5a355ce2ac36c3adb7db3de31b01e7cba22cc71b
dd130609 <danieldmejias@gmail.com>
2025-07-08 14:54:03 -0500

    base de datos actualizacion

--- proyecto/empleado.php
+++ proyecto/empleado.php
@@ -173,7 +173,7 @@ $result = mysqli_query($conexion, $sql);
 while ($mostrar = mysqli_fetch_array($result)) {
 ?>
     <tr>
-        <td><?php echo $mostrar['id_empleado']; ?></td>
+        <td><?php echo $mostrar['id_usuario']; ?></td>
         <td><?php echo $mostrar['id_rol']; ?></td>
         <td><?php echo $mostrar['nombres']; ?></td>
         <td><?php echo $mostrar['apellidos']; ?></td>
@@ -182,10 +182,10 @@ while ($mostrar = mysqli_fetch_array($result)) {
         <td><?php echo $mostrar['telefono']; ?></td>
         <td><?php echo $mostrar['documento']; ?></td>
         <td>
-            <a href="editar_empleado.php?id=<?php echo $mostrar['id_empleado'];?> &id_rol=<?php echo $mostrar['id_rol'];?> &nom=<?php echo $mostrar['nombres'];?> &apell=<?php echo $mostrar['apellidos'];?>  &email=<?php echo $mostrar['correo'];?>  &tel=<?php echo $mostrar['telefono'];?> &docu=<?php echo $mostrar['documento'];?>" class="boton-edi">Editar</a>
+            <a href="editar_empleado.php?id=<?php echo $mostrar['id_usuario'];?> &id_rol=<?php echo $mostrar['id_rol'];?> &nom=<?php echo $mostrar['nombres'];?> &apell=<?php echo $mostrar['apellidos'];?>  &email=<?php echo $mostrar['correo'];?>  &tel=<?php echo $mostrar['telefono'];?> &docu=<?php echo $mostrar['documento'];?>" class="boton-edi">Editar</a>
         </td>
         <td>
-            <a href="./php/eliminarEmp.php?id=<?php echo $mostrar['id_empleado']; ?>" class="boton" onclick="return confirm('¿Estás seguro de que quieres eliminar este empleado?');">Eliminar</a>
+            <a href="./php/eliminarEmp.php?id=<?php echo $mostrar['id_usuario']; ?>" class="boton" onclick="return confirm('¿Estás seguro de que quieres eliminar este empleado?');">Eliminar</a>
         </td>
     </tr>
 <?php
